f127253a2a92a559cfe9b8e061bcef88a5c8ece4
Aleksey Titov <talexden@gmail.com>
2021-08-05 08:14:38 +0300

    работа над ошибками, рефакторинг

--- src/main.js
+++ src/main.js
@@ -1,4 +1,60 @@
-import {renderPage, renderPopup} from './';
+import {createCatalogTemplate} from './view/catalog.js';
+import {createCatalogListTemplate} from './view/catalog-list.js';
+import {createCardTemplate} from './view/card.js';
+import {createShowMoreButtonTemplate} from './view/show-more-button.js';
+import {createPopupTemplate} from './view/popup.js';
+import {createPopupTopContainer} from './view/popup-top-container.js';
+import {createFilmDetalesRow} from './view/film-detales-row.js';
+import {createPopupBottomContainer} from './view/popup-bottom-container.js';
+import {CreateCommetnTemplate} from './view/commetn.js';
+import {render} from './utils.js';
 
-renderPage();
-renderPopup();
+const MAIN_LIST_TITLE = 'All movies. Upcoming';
+const MAIN_LIST_SIZE = 5;
+const TOP_LIST_TITLE = 'Top rated';
+const TOP_LIST_SIZE = 2;
+const MOST_COMMENTED_LIST_TITLE = 'Most commented';
+const MOST_COMMENTED_LIST_SIZE = 2;
+const FILM_DETAILES_COUNT = 7;
+
+const body = document.querySelector('body');
+const main = body.querySelector('.main');
+const footer = body.querySelector('.footer');
+
+
+render(main, createCatalogTemplate(), 'beforeend');
+
+
+const getMultiplyStrings = (string, number) => {
+  let multiplyStrings = '';
+  for (let i = 0; i < number; i++) {
+    multiplyStrings += string;
+  }
+  return multiplyStrings;
+};
+
+
+const catalog = main.querySelector('.films');
+let catalogList;
+
+const renderCatalogList =(listTitle, listSize, modifier) => {
+  render(catalog, createCatalogListTemplate(listTitle, modifier), 'afterbegin');
+  catalogList = catalog.querySelector('.films-list__container');
+  render(catalogList, getMultiplyStrings(createCardTemplate(), listSize), 'beforeend');
+};
+
+
+renderCatalogList(MOST_COMMENTED_LIST_TITLE, MOST_COMMENTED_LIST_SIZE,'films-list--extra');
+renderCatalogList(TOP_LIST_TITLE, TOP_LIST_SIZE,'films-list--extra');
+renderCatalogList(MAIN_LIST_TITLE, MAIN_LIST_SIZE);
+render(catalogList, createShowMoreButtonTemplate(), 'beforeend');
+
+
+render(footer, createPopupTemplate(), 'afterend');
+const filmDetailsInner = document.querySelector('.film-details__inner');
+render(filmDetailsInner, createPopupTopContainer(), 'beforeend');
+const filmDetailsTable = filmDetailsInner.querySelector('.film-details__table');
+render(filmDetailsTable, getMultiplyStrings(createFilmDetalesRow(), FILM_DETAILES_COUNT), 'beforeend');
+render(filmDetailsInner, createPopupBottomContainer(), 'beforeend');
+const filmDetailsCommentsList = filmDetailsInner.querySelector('.film-details__comments-list');
+render(filmDetailsCommentsList, getMultiplyStrings(CreateCommetnTemplate(), 1), 'beforeend');
--- src/utils/index.js
+++
@@ -1,3 +0,0 @@
-export * from './constants.js';
-export * from './path.js';
-export * from './utils.js';
